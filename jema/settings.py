a34dd355d1cfa78b3a5afa72ca69cceb5716117c
jensenperez <artperezjensen@gmail.com>
2022-12-27 20:30:04 -0500

    cambiando debig a False

--- jema/settings.py
+++ jema/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!cwbkko+o-0b%vdy-ymbfhru0pj)rqb^f=(+p1f%t2igr^dggl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
